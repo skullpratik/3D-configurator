8cf48e0d840290fee386a65ab69a4e750b1d286f
skullpratik <phoolchandkanojiya9819@gmail.com>
2025-08-10 00:04:34 +0530

    Fix build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: '.', // Looks for index.html in root
+  publicDir: 'public', // Your other static files
   plugins: [react()],
+  server: {
+    port: 3000
+  }
 })
