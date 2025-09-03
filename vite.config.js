90cbaf5f42dfeb3aceab365af47ab420c6774853
skullpratik <phoolchandkanojiya9819@gmail.com>
2025-09-03 18:25:44 +0530

    p

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   root: '.', // Looks for index.html in root
   publicDir: 'public', // Your other static files
+    base: "/", // ensures correct asset paths on Vercel
   plugins: [react()],
   server: {
     port: 3000
